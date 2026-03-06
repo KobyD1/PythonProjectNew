dc2c2b3033575dc72add349065f828127fde00c1
dkdk1 <dkdk100@walla.co.il>
2026-03-06 12:02:35 +0200

    106 first commit

---
+++ ics/106/casting_example.py
@@ -0,0 +1,11 @@
+
+
+num_1 = '12'
+num_1_as_int = int(num_1)  # how to convert to int
+sum = num_1_as_int+4
+num_2 = 345
+num_2_as_str = str(num_2)   # how to convert to str
+num_2_final = num_2_as_str.replace("5","5$")
+
+print("Test end")
+
