91056f7b6538bf1776074ff479d0566069ffd276
dkdk1 <dkdk100@walla.com>
2026-03-15 20:29:33 +0200

    loop examples

---
+++ ics/106/multi_table.py
@@ -0,0 +1,16 @@
+
+
+for i in range (12):
+    print (f"{3*i}={i}* 3")
+
+
+for i in range (12):
+
+    num_1 = 12-i
+    print (f"the summery of {i} and {num_1} is 12")
+
+
+
+
+
+
---
+++ ics/106/while_loop_example.py
@@ -0,0 +1,5 @@
+i=0
+# it is mandatory  to define when to stop the loop
+while (i<5):
+    print (i)
+    i = i +1
