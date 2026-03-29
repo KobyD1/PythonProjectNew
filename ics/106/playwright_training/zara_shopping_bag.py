45b804541b68592152f0d9fce6fe2f9e62e85365
dkdk1 <dkdk100@walla.com>
2026-03-29 18:36:20 +0300

    get by link

---
+++ ics/106/playwright_training/zara_shopping_bag.py
@@ -0,0 +1,26 @@
+import time
+
+from playwright.sync_api import sync_playwright, expect
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto("https://www.zara.com/il/en/")
+
+    shopping_button = page.locator("[class='layout-actionable link']").all()[0]
+    shopping_button = page.locator("[class*='layout-actionabl']").all()[0]
+
+
+    shopping_button_text =shopping_button.inner_text()
+    index=shopping_button_text.index("bag")
+    count = shopping_button_text[index+3:]
+    count_1= shopping_button_text[-1:]
+
+
+
+
+
+
+
+    print ("test end")
+
+    browser.close()
