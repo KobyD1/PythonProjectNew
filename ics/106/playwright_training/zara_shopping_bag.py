b9e03789c25037753b8da2c0fe79e508069b2a65
dkdk1 <dkdk100@walla.com>
2026-03-29 19:17:54 +0300

    assert

--- ics/106/playwright_training/zara_shopping_bag.py
+++ ics/106/playwright_training/zara_shopping_bag.py
@@ -7,13 +7,13 @@ with sync_playwright() as p:
     page.goto("https://www.zara.com/il/en/")
 
     shopping_button = page.locator("[class='layout-actionable link']").all()[0]
-    shopping_button = page.locator("[class*='layout-actionabl']").all()[0]
+    # shopping_button = page.locator("[class*='layout-actionabl']").all()[0]
 
-
+    time.sleep(3)
     shopping_button_text =shopping_button.inner_text()
-    index=shopping_button_text.index("bag")
-    count = shopping_button_text[index+3:]
     count_1= shopping_button_text[-1:]
+    print ("0 was found in shopping_button text")
+    assert count_1 == "1","1 was not found in shopping_button text"
 
 
 
