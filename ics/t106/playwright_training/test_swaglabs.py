4e332d4553842efee91191a1f98bbd68d68e89ec
dkdk1 <dkdk100@walla.com>
2026-04-17 10:53:18 +0300

    adding swaglabs by css

--- ics/t106/playwright_training/test_swaglabs.py
+++ ics/t106/playwright_training/test_swaglabs.py
@@ -7,6 +7,21 @@ def test_login_by_css(setup_playwright):
     print ("into swag labs")
     page =setup_playwright
     page.goto("https://www.saucedemo.com/")
+    user = page.locator("input[class*='input_error']").first
+    passowrd = page.locator("input[class*='input_error']").last
+
+    user.click()
+    user.fill("standard_user")
+
+    passowrd.click()
+    passowrd.fill("secret_sauce")
+
+    login = page.locator("input[class='submit-button btn_action']")
+    login.click()
+
+
+    print ("test point")
+
 
 
 
