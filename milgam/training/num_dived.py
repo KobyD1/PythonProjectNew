2a35302d0cad040a8947a6db7b37d0aefd923bb2
dkdk1 <dkdk100@walla.com>
2026-06-14 14:09:45 +0300

    training

---
+++ milgam/training/num_dived.py
@@ -0,0 +1,21 @@
+# for i in range(1,200):
+#
+#     if (i % 7 ==0):
+#         print(i)
+
+print ("********************")
+for num in range(0,20):
+
+    if (num ==3):
+        continue     # how to jump to the next interaction
+
+    if (num ==5):
+        break
+
+    print("********************")
+    print(num)
+    # end of loop
+
+
+print ("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+
