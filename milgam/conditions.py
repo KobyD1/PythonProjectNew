c00cf87d9baabe26c06cabecd2fd9644de570ce2
dkdk1 <dkdk100@walla.com>
2026-06-14 11:50:57 +0300

    training

--- milgam/conditions.py
+++ milgam/conditions.py
@@ -1,15 +1,32 @@
-num_1 = 3
+
+num_3 =1
+num_1 = 5
 num_2 = 4
-sum = num_1 + num_2
+sum = (num_1 + num_2)* num_3
+sum =7
+# analyze the value of sum
+if (sum == 7):
+    print("sum equals 7")
+    sum_1 = sum *2
+    sum_2 = sum * 3
+    print(f"sum equals {sum}")
+
+elif (sum > 10):   #
+    sum = sum + 10
 
 
-if (sum>10):
-    sum = sum +10
+elif (sum > 20) :
+    sum = sum + 20
+
+elif (sum > 30):
+    sum = sum + 30
 
 else:
-    sum = sum -10
+    sum = sum + 40
 
-print ("end")
+# analyze the value of sum_2
 
+if (sum_2 >10 ):
+    print ("sum2 is more VS 10")
 
-
+print("end")
