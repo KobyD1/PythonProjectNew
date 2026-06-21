c23eee59943bee75fc30d9530ad9ec076deb4bf1
dkdk1 <dkdk100@walla.com>
2026-06-21 12:34:25 +0300

    add under over

---
+++ milgam/training/sum_neg_positive.py
@@ -0,0 +1,18 @@
+nums = [3,5,6,-2,34,-30,0,-4,-34]
+sum_p = 0
+sum_n = 0
+
+for num in nums:
+    if num ==0:
+        break
+
+    # else:
+    if num >0:
+        sum_p = sum_p+num
+
+    if num<0 :
+        sum_n = sum_n + num
+
+
+
+
