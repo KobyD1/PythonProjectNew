902d19522f0508cda075e3640e929ea5ff5427aa
USER <dkdk100@walla.com>
2026-06-25 14:06:21 +0300

    add scripts v1

---
+++ winner_final/all_no_filter_v1.bat
@@ -0,0 +1,13 @@
+@echo off
+set timestamp=%date:~-4%-%date:~3,2%-%date:~0,2%_%time:~0,2%-%time:~3,2%
+set timestamp=%timestamp: =0%
+set PYTHONUTF8=1
+set PYTHONIOENCODING=utf-8
+set project_root=C:\Users\USER\PycharmProjects\PythonProjectNew
+set script_path=%project_root%\winner_final
+cd /d %script_path%
+set PYTHONPATH=%project_root%
+
+@REM pip install pandas
+@REM playwright install
+"C:\Users\USER\AppData\Local\Programs\Python\Python313\python.exe" table_game_no_filter.py
