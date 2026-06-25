97a82fc0c3b6343f8d19011f3b9797ce5bc6b742
dkdk1 <dkdk100@walla.com>
2026-06-25 14:12:13 +0300

    fix issue with - at team name

---
+++ winner_final/wnba_final.bat
@@ -0,0 +1,13 @@
+@echo off
+set timestamp=%date:~-4%-%date:~3,2%-%date:~0,2%_%time:~0,2%-%time:~3,2%
+set timestamp=%timestamp: =0%
+set PYTHONUTF8=1
+set PYTHONIOENCODING=utf-8
+
+set project_root=C:\Users\dkdk1\PycharmProjects\PythonProjectNew
+set script_path=%project_root%\winner_final
+
+cd /d %script_path%
+
+set PYTHONPATH=%project_root%
+wnba_game_final.py > "C:\Users\Public\Winner\results\output_%timestamp%.txt" 2>&1
