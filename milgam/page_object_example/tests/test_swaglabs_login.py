0538199c31ef9a605e17f5d3ee7c5d5868ffc242
dkdk1 <dkdk100@walla.com>
2026-07-16 14:37:37 +0300

    add print to pdf without consol

--- milgam/page_object_example/tests/test_swaglabs_login.py
+++ milgam/page_object_example/tests/test_swaglabs_login.py
@@ -1,8 +1,15 @@
+from milgam.page_object_example.pages.product_page import ProductPage
+from milgam.page_object_example.pages.welcome_page import WelcomePage
 
 
 class TestSwaglabsLogin():
 
     def test_swaglabs_login_positive(self,setup_playwright_swaglabs):
-        page = setup_playwright_swaglabs
+        # page = setup_playwright_swaglabs
+        product_page = ProductPage()
+        welcome_page = WelcomePage()
+
+        welcome_page.login_by_user_password()
+
 
         print ("test end")
