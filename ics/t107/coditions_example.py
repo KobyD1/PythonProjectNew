5fadfe3816cd7c2361333ba1930add4ad43eb729
dkdk1 <dkdk100@walla.com>
2026-08-20 20:37:44 +0300

    add agents files

---
+++ ics/t107/coditions_example.py
@@ -0,0 +1,16 @@
+num1 = 2
+num2=6
+res= num1*num2
+
+
+
+if (res>8):
+    print ("the result of multiple is higher")
+    res = res+8
+    print (f"the new value is {res}")
+
+
+else:
+    print ("the result is lower than 8 ")
+
+
---
+++ ics/t107/loop_example.py
@@ -0,0 +1,9 @@
+summery = 0
+
+for i in range (0,3):
+    print (f"the value of i is {i}")
+    summery = summery+i
+    print   (f" The value of summery is {summery}")
+
+print ("test end")
+
