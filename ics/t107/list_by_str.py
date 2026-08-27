a02123bff8f2b0206dff27609d6578ddd20dcdcb
dkdk1 <dkdk100@walla.com>
2026-08-27 20:19:52 +0300

    add agents files

---
+++ ics/t107/list_by_str.py
@@ -0,0 +1,8 @@
+cities = ["Madrid","London","Paris","Berlin","Lod","Tel-Aviv"]
+# capital_city = "Jerusalem"
+cities[4] = "Haifa"
+for city in cities:
+    print (city)
+    if city == "Paris":
+        print ("Paris found")
+        break
