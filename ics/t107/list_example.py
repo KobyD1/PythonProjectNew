e858fe17f7981c8fbe279c7659bfe8ba8ca11790
dkdk1 <dkdk100@walla.com>
2026-08-27 18:28:12 +0300

    add agents files

--- ics/t107/list_example.py
+++ ics/t107/list_example.py
@@ -2,3 +2,10 @@ numbers = [23,33,65,163,5,3]
 
 for number in numbers:
     print(number)
+    if number > 10:
+        print ("Number is greater than 10")
+
+    else:
+        print ("Number is less than 10")
+    result = number * 10
+    print(f"the result is {result}")
