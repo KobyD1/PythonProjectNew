24782d73916ed3d420139d929697c784e8b5eb1b
dkdk1 <dkdk100@walla.com>
2026-08-30 18:26:40 +0300

    add agents files

--- ics/t107/summery_calc.py
+++ ics/t107/summery_calc.py
@@ -1,18 +1,14 @@
 # find the summery of positive numbers
 # and the summery of the negative numbers
 # at 2 diffrent variables
-# if the list contains 0 - all activity stop
-# if the list contains 100 - all activity will be not done
 
-nums = [1,4,6,-4,100,-10,3,0,-5,7,9]
+nums = [1,4,6,-4,-10,3,0,-5,7,9]
 positive_sum = 0
 negative_sum = 0
 
 for num in nums:
     if num == 0:
         break
-    elif num ==100:
-        continue
     elif num > 0:
         positive_sum = positive_sum + num
 
