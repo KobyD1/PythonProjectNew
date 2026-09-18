5e02bf9f1c472abe7e23e0a28b3cddee184b8d43
dkdk1 <dkdk100@walla.com>
2026-09-18 18:17:19 +0300

    fix for single API read

--- winner_final/football_under_over_final.py
+++ winner_final/football_under_over_final.py
@@ -8,22 +8,42 @@ football_data_api = FootballDataApi()
 playwright_main = PlaywrightMainUI()
 files_utils = FilesUtils()
 algo_utils = AlgoUtils()
-table_data =playwright_main.set_telesport_page( 2,1,"spain")
 results_not_sorted =[]
+
+table_data =playwright_main.set_telesport_page( 2,2,"spain")
 for data in table_data:
     print (f"******* Game : {data["game"]} - {data['team_a']} VS {data['team_b']} *******")
     if data["description"] == '2 Teams Under/Over 2-3 Range':
 
-        teams_data_excel= files_utils.get_teams_names_from_excel("spain.xlsx",data.get("team_a"),data.get("team_b") )
+        teams_data_excel= files_utils.get_teams_names_from_excel("football_all.xlsx",data.get("team_a"),data.get("team_b") )
         teams_data = football_data_api.get_teams_data("PD",[teams_data_excel.get("team_a"),teams_data_excel.get("team_b")])
 
         score , favorite ,avg_total = algo_utils.calc_football_under_over_algo(teams_data[0], teams_data[1],data["description"])
-        bet = data[f"bet{favorite}"]
-        results_not_sorted.append({"bet":bet,"score": score, "favorite": favorite,"plan": data["game"],'game': data["game_in_program"],"avg_total":avg_total})
-    results_sorted = sorted(results_not_sorted, key=lambda x: x["score"], reverse=True)
+        if (favorite != 0 ):
+            bet = data[f"bet{favorite}"]
+            if favorite == 3: favorite = "X"
+            results_not_sorted.append({"bet":bet,"score": score, "favorite": favorite,"plan": data["game"],'game': data["game_in_program"],"avg_total":avg_total})
+
+table_data =playwright_main.set_telesport_page( 2,1,"england")
+
+for data in table_data:
+    print (f"******* Game : {data["game"]} - {data['team_a']} VS {data['team_b']} *******")
+    if data["description"] == '2 Teams Under/Over 2-3 Range':
+
+        teams_data_excel= files_utils.get_teams_names_from_excel("football_all.xlsx",data.get("team_a"),data.get("team_b") )
+        teams_data = football_data_api.get_teams_data("PL",[teams_data_excel.get("team_a"),teams_data_excel.get("team_b")])
+
+        score , favorite ,avg_total = algo_utils.calc_football_under_over_algo(teams_data[0], teams_data[1],data["description"])
+        if (favorite != 0 ):
+            bet = data[f"bet{favorite}"]
+            if favorite == 3: favorite = "X"
+            results_not_sorted.append({"bet":bet,"score": score, "favorite": favorite,"plan": data["game"],'game': data["game_in_program"],"avg_total":avg_total})
+
+
+
 headers =  ["Favorite", "Game", "Plan", "Score", "Bet", "Avg Goals"]
-files_utils.print_results(results_sorted,headers,"** Summery Results for Under-Over 2-3 goals **")
 results_sorted = sorted(results_not_sorted, key=lambda x: x["score"], reverse=True)
-    # files_utils.wrote_to_text_file(table_data, PATH_PROGRAM_TEXT, "program")
+files_utils.print_results(results_sorted,headers,"** Summery Results for Under-Over 2-3 goals **")
+
 
 print ("********  End  *********")
